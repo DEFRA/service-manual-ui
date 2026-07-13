3ad0a04dfa14db652c39348bee82e2f3f37172c7
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2026-07-13 15:51:16 +0100

    Override axios default proxy

--- src/notify/notify-client.js
+++ src/notify/notify-client.js
@@ -1,3 +1,4 @@
+import axiosClient from 'axios'
 import { NotifyClient } from 'notifications-node-client'
 
 /**
@@ -18,8 +19,7 @@ import { NotifyClient } from 'notifications-node-client'
 function createNotifyClient (apiKey) {
   const client = new NotifyClient(apiKey)
 
-  // ensure axios does not use it's own proxy. Global proxy envs are used by default.
-  client.setProxy(false)
+  axiosClient.defaults.proxy = false
 
   return client
 }
